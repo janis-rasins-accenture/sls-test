03b389778d0cf17ff7921b8bbb04e10cef2e07a5
Janis Rasins <extern.janis.rasins@vwfs.io>
2023-04-06 11:37:52 +0300

    Add simple validation

--- users.js
+++ users.js
@@ -1,14 +1,65 @@
-export {putItem} from './dynamodb/putItem'
+import { putItem } from './aws/dynamodb/putItem.js'
+import { v4 as uuidv4 } from 'uuid'
 
-export const createUser = () => {
+export const createUser = async (event) => {
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(
+                {
+                    message: "No body!",
+                },
+                null,
+                2
+            ),
+        }
+    }
+    const body = JSON.parse(event.body)
+    if (!body.firstName) {
+        console.log('body: ', body)
+        return {
+            statusCode: 400,
+            body: JSON.stringify(
+                {
+                    message: "No firstName!",
+                },
+                null,
+                2
+            ),
+        }
+    }
+    const uuid = uuidv4()
     const params = {
         TableName: process.env.TABLE_NAME_USERS,
         Item: {
-            userId: "User_1",
-            firstName: "Test",
-            isActive: true
+            userId: uuid,
+            firstName: body.firstName,
+            isActive: 1
         }
     }
-    const result = putItem(params)
-    console.log(result)
+    const result = await putItem(params)
+    if (result.success) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: "Success!",
+                    userId: uuid
+                },
+                null,
+                2
+            ),
+        }
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(
+                {
+                    message: "No firstName!",
+                },
+                null,
+                2
+            ),
+        }
+    }
 }
